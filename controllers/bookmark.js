db068c6b7384ac355fe1c288b437ab31fe030f37
Kenny Nguyen <knguyen111601@gmail.com>
2021-11-13 11:56:07 -0800

    created update and delete route

--- controllers/bookmark.js
+++ controllers/bookmark.js
@@ -31,6 +31,24 @@ router.post("/bookmarks", async (req, res) =>{
     }
 })
 
+// Update Route - put
+router.put("/bookmarks/:id", async (req, res) =>{
+    try{
+        res.json(await Bookmark.findByIdAndUpdate(req.params.id, req.body, {new: true}))
+    } catch (error) {
+        res.status(400).json(error)
+    }
+})
+
+// Destroy Route - delete
+router.delete("/bookmarks/:id", async (req, res)=>{
+    try{
+        res.json(await Bookmark.findByIdAndRemove(req.params.id))
+    } catch (error) {
+        res.status(400).json(error)
+    }
+})
+
 ///////////////////////////////////////////////////////
 // Export Router
 ///////////////////////////////////////////////////////
